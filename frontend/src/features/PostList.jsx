bdee15619da4d4aad3d00b5691aff808cd484b43
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 13:59:25 +0300

    card

--- frontend/src/features/PostList.jsx
+++ frontend/src/features/PostList.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function PostList({ post }) {
-  return <div className="text-center">{post.title}</div>;
+  return (
+    <div className="h-12 mt-2 border rounded border-indigo-600 px-3 text-left flex items-center">
+      {post.title}
+    </div>
+  );
 }
 
 export default PostList;
