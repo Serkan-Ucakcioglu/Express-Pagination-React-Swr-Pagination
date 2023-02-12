f39a739696f2803115600663eda8982e6d15baba
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 12:49:44 +0300

    use route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./config/connectDb");
+const postRoute = require("./router/postRoute");
 require("dotenv").config();
 connectDb();
 
 app.get("/", (req, res) => {
   res.send("hello from simple server :)");
 });
+app.use("/", postRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () =>
