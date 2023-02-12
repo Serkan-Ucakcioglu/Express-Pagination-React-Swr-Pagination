f0cd379c9adaf2d5d7ac43106433091e611aa695
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 12:50:57 +0300

    controller

---
+++ backend/controllers/postControllers.js
@@ -0,0 +1,10 @@
+const Post = require("../model/postSchema");
+
+const getPost = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = getPost;
