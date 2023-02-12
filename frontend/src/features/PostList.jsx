88bff48a07500023bf713222ff6eb8d011b618b5
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 14:09:53 +0300

    id add

--- frontend/src/features/PostList.jsx
+++ frontend/src/features/PostList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PostList({ post }) {
   return (
     <div className="h-12 mt-2 border rounded border-indigo-600 px-3 text-left flex items-center">
-      {post?.title}
+      {post?.id}. {post?.title}
     </div>
   );
 }
