a7f5f56cc2502f8f761cae486c5cd6d5b87a85fa
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 12:51:23 +0300

    getPost add route

--- backend/router/postRoute.js
+++ backend/router/postRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const getPost = require("../controllers/postControllers");
 
 const router = express.Router();
+
+router.get("/post", getPost);
 
 module.exports = router;
