b8b5b9bda40436a8e29280227cb8525692f03a1e
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-12 12:55:51 +0300

    post controller

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -2,6 +2,19 @@ const Post = require("../model/postSchema");
 
 const getPost = async (req, res) => {
   try {
+    const { page = 1, limit = 10 } = req.query;
+    const postList = await Post.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    const allPostCount = await Post.countDocuments();
+
+    res.json({
+      postList,
+      totalPages: Math.ceil(allPostCount / limit),
+      currentPage: page,
+    });
   } catch (error) {
     res.status(500).json(error);
   }
--- backend/router/postRoute.js
+++ backend/router/postRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const getPost = require("../controllers/postControllers");
-
 const router = express.Router();
 
 router.get("/post", getPost);
