f79f015f4b34f506f037da4f0a1c70fb518c73d9
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-13 14:09:35 +0300

    border update

--- frontend/src/features/PostList.jsx
+++ frontend/src/features/PostList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PostList({ post }) {
   return (
-    <div className="h-12 mt-2 border rounded border-indigo-600 px-3 text-left flex items-center">
+    <div className="h-12 mt-2 border-2 rounded border-indigo-600 px-3 text-left flex items-center">
       {post?.id}. {post?.title}
     </div>
   );
