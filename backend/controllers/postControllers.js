ba06032449458222bc4c25044016ab81de0343e4
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-13 11:35:56 +0300

    updated

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -2,16 +2,18 @@ const Post = require("../model/postSchema");
 
 const getPost = async (req, res) => {
   try {
+    //pagination details
     const { page = 1, limit = 10 } = req.query;
     const postList = await Post.find()
       .limit(limit * 1)
       .skip((page - 1) * limit)
       .exec();
 
+    // all count
     const allPostCount = await Post.countDocuments();
 
     res.json({
-      postList,
+      postList, // list
       totalPages: Math.ceil(allPostCount / limit),
       currentPage: page,
     });
