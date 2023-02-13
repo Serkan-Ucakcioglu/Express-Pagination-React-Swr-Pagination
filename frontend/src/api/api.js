3dc57b1738bacfa8b658b4e2ec0ae5df4e202cd5
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-13 11:27:55 +0300

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+//api
 export const api = axios.create({
   baseURL: "http://localhost:3001/",
 });
 
+//get post
 export const getPost = async (limit, page) => {
   const { data } = await api.get(
     `/post?limit=${limit || 10}&page=${page || 1}`
