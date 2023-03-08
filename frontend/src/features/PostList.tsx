6d6bdd384f960d68f8cf25bb94c01f3f47b5d38c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-08 11:39:06 +0300

    update

--- frontend/src/features/PostList.tsx
+++ frontend/src/features/PostList.tsx
@@ -7,11 +7,9 @@ interface IPost {
 
 function PostList({ post }: IPost) {
   return (
-    <>
-      <div className="h-12 mt-2 border-2 rounded border-indigo-600 px-3 text-left flex items-center">
-        {post?.id}. {post?.title}
-      </div>
-    </>
+    <div className="h-12 mt-2 border-2 rounded border-indigo-600 px-3 text-left flex items-center">
+      {post?.id}. {post?.title}
+    </div>
   );
 }
 
