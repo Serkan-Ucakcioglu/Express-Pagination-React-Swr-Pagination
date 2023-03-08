08cfa6c083314b00ba4919046899be6e68c5f962
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-08 14:51:17 +0300

    indent

--- frontend/src/features/PostList.tsx
+++ frontend/src/features/PostList.tsx
@@ -4,7 +4,6 @@ import { Post } from "./PagiButton";
 interface IPost {
   post: Post;
 }
-
 function PostList({ post }: IPost) {
   return (
     <div className="h-12 mt-2 border-2 rounded border-indigo-600 px-3 text-left flex items-center">
